71e3d0b310e49c157ac662744c68c990679b9fd3
Tanushree Aggarwal <47485195+shreetanu@users.noreply.github.com>
2021-07-25 12:21:40 +0530

    Context Provider added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import { QuizContextProvider } from '../store/quiz-context'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return
+  (
+    <QuizContextProvider>
+    <Component {...pageProps} />
+    </QuizContextProvider>
+  )
 }
 
 export default MyApp
