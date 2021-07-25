8905c4cbf8a8e6b3bfc9643f997048ec8ee8e80d
Yatin Jain <rokstaryatin2012@gmail.com>
2021-07-25 23:57:16 +0530

    image element in index using next/image

--- pages/index.js
+++ pages/index.js
@@ -1,78 +1,90 @@
-import { useState } from 'react'
-import { useRouter } from 'next/router'
-import ErrorMessage from '../components/ErrorMessage';
-import Head from 'next/head'
-import classes from '../styles/index.module.css'
-import Header from '../components/Header'
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { useRouter } from "next/router";
+import ErrorMessage from "../components/ErrorMessage";
+import classes from "../styles/index.module.css";
+import Header from "../components/Header";
+import Image from "next/image";
+
 function HomePage() {
-  const [name, setName] = useState('');
-  const [difficulty, setDifficulty] = useState("");
-  const [error, setError] = useState(false);
+	const [name, setName] = useState("");
+	const [difficulty, setDifficulty] = useState("");
+	const [error, setError] = useState(false);
 
-  const handleChange = (text) => {
-    if (text) {
+	const handleChange = text => {
+		if (text) {
+			setName(text);
+		} else {
+			setName("");
+		}
+	};
+	const router = useRouter();
+	let api = "";
+	const handleSubmit = () => {
+		if (!difficulty || !name || difficulty === "select") {
+			setError(true);
+			return;
+		} else {
+			setError(false);
+			//for test
+			api =
+				"https://opentdb.com/api.php?amount=15&category=18&difficulty=" +
+				difficulty +
+				"&type=multiple";
+			router.push({
+				pathname: "/quiz",
+				query: { link: api },
+			});
+		}
+	};
+	return (
+		<div className={classes.content}>
+			<div className={classes.settings}>
+				<div className={classes.settings__select}>
+					<Header></Header>
+					{error && <ErrorMessage>Please Fill all the feilds</ErrorMessage>}
+					<input
+						placeholder='User Name'
+						type='text'
+						style={{ marginBottom: 25 }}
+						className={classes.elements}
+						id='name'
+						value={name}
+						onChange={event => handleChange(event.target.value)}
+					/>
 
-      setName(text);
-    } else {
-      setName('');
-    }
-  };
-  const router = useRouter()
-  let api = '';
-  const handleSubmit = () => {
-    if (!difficulty || !name || difficulty === 'select') {
-      setError(true);
-      return;
-    } else {
-      setError(false);
-      //for test 
-      api = 'https://opentdb.com/api.php?amount=15&category=18&difficulty=' + difficulty+'&type=multiple';
-      router.push({
-        pathname: '/quiz',
-        query: { link: api }
-      })
+					<select
+						value={difficulty}
+						className={classes.elements}
+						style={{ marginBottom: 30 }}
+						onChange={e => setDifficulty(e.target.value)}>
+						<option key='select' value='select'>
+							Difficulty Level
+						</option>
+						<option key='Easy' value='easy'>
+							Easy
+						</option>
+						<option key='Medium' value='medium'>
+							Medium
+						</option>
+						<option key='Hard' value='hard'>
+							Hard
+						</option>
+					</select>
 
-    }
-  };
-  return (
-    <div className={classes.content} >
-      <div className={classes.settings}>
-        <div className={classes.settings__select}>
-          <Header></Header>
-          {error && <ErrorMessage>Please Fill all the feilds</ErrorMessage>}
-          <input
-            placeholder="User Name"
-            type="text"
-            style={{ marginBottom: 25 }}
-            className={classes.elements}
-            id="name"
-            value={name}
-            onChange={(event) => handleChange(event.target.value)}
-          />
-
-
-          <select value={difficulty}
-
-            className={classes.elements}
-            style={{ marginBottom: 30 }}
-            onChange={(e) => setDifficulty(e.target.value)}>
-            <option key="select" value="select" >Difficulty Level</option>
-            <option key="Easy" value="easy">Easy</option>
-            <option key="Medium" value="medium">Medium</option>
-            <option key="Hard" value="hard">Hard</option>
-
-          </select>
-            
-          <button   className={classes.button} onClick={handleSubmit}>Start Quiz</button>
-         
-        </div>
-      </div>
-      <img src='/quiz.svg' className={classes.banner}></img>
-    </div>
-
-
-  )
+					<button className={classes.button} onClick={handleSubmit}>
+						Start Quiz
+					</button>
+				</div>
+			</div>
+			<div className={classes.banner}>
+				<Image
+					src='/quiz.svg'
+					width={720}
+					height={480}
+					alt='Person alongside board'
+				/>
+			</div>
+		</div>
+	);
 }
 export default HomePage;
-
