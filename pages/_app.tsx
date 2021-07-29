3a22e32f9637cc3e872990def630ae37f6bec2b9
jainvaidehi <54447956+jainvaidehi@users.noreply.github.com>
2021-07-29 22:37:38 +0530

    MUI added to Index page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,17 @@ import { QuizContextProvider } from "../store/quiz-context";
 import {QueryClient, QueryClientProvider} from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools'
 import type { AppProps} from 'next/app'
+
+import React from "react";
+import Footer from "../components/Footer";
 function MyApp({ Component, pageProps }:AppProps) {
 
 	const queryClient = new QueryClient();
 	return <QueryClientProvider client={queryClient}>
 			<QuizContextProvider>
+
 				<Component {...pageProps} />
+				<Footer/>
 			</QuizContextProvider>
 			</QueryClientProvider>;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,29 @@
 import { useState, useContext } from "react";
 import { useRouter } from "next/router";
 import ErrorMessage from "../components/ErrorMessage";
-
-import Header from "../components/Header";
+import React from 'react';
+import ShowChartIcon from '@material-ui/icons/ShowChart';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import Input from '@material-ui/core/Input';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import Button from '@material-ui/core/Button';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import Image from "next/image";
 import QuizContext from "../store/quiz-context";
 import classes from '../styles/index.module.css'
+import RotateRightIcon from '@material-ui/icons/RotateRight'
+
+
 function HomePage() {
+	interface State {
+		password: string;
+		showPassword: boolean;
+	}
+
 	const ctx = useContext(QuizContext);
 	const [difficulty, setDifficulty] = useState("");
 	const [error, setError] = useState(false);
@@ -19,58 +36,117 @@ function HomePage() {
 		}
 	};
 	const router = useRouter();
-	//let api = "";
+	const [values, setValues] = React.useState<State>({
+		password: '',
+		showPassword: false,
+	});
+	const handleClickShowPassword = () => {
+		setValues({ ...values, showPassword: !values.showPassword });
+	};
+	const handleChangepass = (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+		setValues({ ...values, [prop]: event.target.value });
+	};
+	const handleReset=()=>{
+		setDifficulty("");
+		setValues({
+			password:'',
+			showPassword: false,
+
+		});
+		ctx.updateUserName("");
+
+	}
 	const handleSubmit = () => {
-		if (!difficulty || !ctx.userName || difficulty === "select") {
+		if (!difficulty || !ctx.userName || difficulty === "select" || !values.password) {
 			setError(true);
 			return;
 		} else {
+
 			setError(false);
 			//for test
+			if( values.password!== "Trivia")
+			{
+
+			}
+			else{
 			router.push({
 				pathname: "/quiz",
 				query: { type: difficulty },
 			});
-		}
+		}}
 	};
 	return (
+		<>
+
 		<div className={classes.content}>
 			<div className={classes.settings}>
 				<div className={classes.settings__select}>
-					<Header/>
 					{error && <ErrorMessage> Please Fill all the fields</ErrorMessage>}
-					<input
+					<Input
 						placeholder='User Name'
-						type='text'
-						style={{ marginBottom: 25 }}
-						className={classes.elements}
 						id='name'
 						value={ctx.userName}
 						onChange={event => handleChange(event.target.value)}
+						startAdornment={
+							<InputAdornment position="start">
+								<PersonOutlineIcon />
+							</InputAdornment>
+						}
 					/>
 
-					<select
-						value={difficulty}
-						className={classes.elements}
-						style={{ marginBottom: 30 }}
-						onChange={e => setDifficulty(e.target.value)}>
-						<option key='select' value='select'>
-							Difficulty Level
-						</option>
-						<option key='Easy' value='easy'>
-							Easy
-						</option>
-						<option key='Medium' value='medium'>
-							Medium
-						</option>
-						<option key='Hard' value='hard'>
-							Hard
-						</option>
-					</select>
+					<Input
+						id="standard-adornment-password"
+						placeholder={"Password"}
+						type={values.showPassword ? 'text' : 'password'}
+						value={values.password}
+						onChange={handleChangepass('password')}
+						endAdornment={
+							<InputAdornment position="end">
+								<IconButton
+									aria-label="toggle password visibility"
+									onClick={handleClickShowPassword}
 
-					<button className={classes.button} onClick={handleSubmit}>
+								>
+									{values.showPassword ? <Visibility /> : <VisibilityOff />}
+								</IconButton>
+							</InputAdornment>
+						}
+					/>
+
+
+
+					<NativeSelect
+						/*className={classes.selectEmpty}*/
+						value={difficulty}
+						onChange={(event) => setDifficulty(event.target.value)}
+						inputProps={{ 'aria-label': 'age' }}>
+						<ShowChartIcon/>
+						<option value="" disabled>
+							Difficulty
+						</option>
+						<option key='Easy' value='easy'>Easy</option>
+						<option key='Medium' value='medium'>Medium</option>
+						<option key='Hard' value='hard'>Hard</option>
+					</NativeSelect>
+<div style={{display:'flex' , justifyContent:'space-between'}} >
+					<Button
+						variant="contained"
+						color="primary"
+						className={classes.button}
+						onClick={handleSubmit}
+						endIcon={<KeyboardArrowRightIcon/>}
+					>
 						Start Quiz
-					</button>
+					</Button>
+					<Button
+						variant="contained"
+						color="primary"
+						className={classes.button}
+						onClick={handleReset}
+					>
+						<RotateRightIcon/>
+					</Button>
+</div>
 				</div>
 			</div>
 			<div className={classes.banner}>
@@ -80,8 +156,11 @@ function HomePage() {
 					height={480}
 					alt='Person alongside board'
 				/>
+
 			</div>
 		</div>
+
+		</>
 	);
 }
 export default HomePage;
